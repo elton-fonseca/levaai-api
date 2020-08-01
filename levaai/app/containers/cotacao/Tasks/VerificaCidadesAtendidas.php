4e55fe2371081500c1d18399937e544401cf5173
Elton Fonseca <elton869@gmail.com>
2020-08-01 08:56:44 -0300

    larastan e inicio verificação cidade

---
+++ levaai/app/containers/cotacao/Tasks/VerificaCidadesAtendidas.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Cotacao\Tasks;
+
+use Cotacao\Repositories\CidadeRepository;
+
+class BuscaBlocos
+{
+    private BlocoRepository $blocoRepository;
+
+    private CidadeRepository $cidadeRepository;
+
+    public function __construct(
+        BlocoRepository $blocoRepository,
+        CidadeRepository $cidadeRepository
+    )
+    {
+        $this->blocoRepository = $blocoRepository;
+        $this->cidadeRepository = $cidadeRepository;
+    }
+
+    public function executar(string $cepOrigem, string $cepDestino): void
+    {
+        $cidadeOrigem = $this->cidadeRepository->buscaPorCodigoMunicipio($cepOrigem);
+        $cidadeDestino = $this->cidadeRepository->buscaPorCodigoMunicipio($cepDestino);
+
+        $atendeCidadeOrigem = '';
+        
+    }
+}
